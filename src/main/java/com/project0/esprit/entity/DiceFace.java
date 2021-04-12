f584d4ef00783e7c0a5f5d492b70703b934edebc
eya122 <bahria.eya@gmail.com>
2021-04-12 14:12:14 +0100

    update diceface

--- src/main/java/com/project0/esprit/entity/DiceFace.java
+++ src/main/java/com/project0/esprit/entity/DiceFace.java
@@ -3,16 +3,20 @@ package com.project0.esprit.entity;
 public class DiceFace {
 
     private int rolledNumber;
-
-    public void setRolledNumber(int rolledNumber) {
+    
+    public DiceFace(int rolledNumber) {
         this.rolledNumber = rolledNumber;
     }
 
     public int getRolledNumber() {
         return rolledNumber;
     }
-
-    public DiceFace(int rolledNumber) {
+    
+    public void setRolledNumber(int rolledNumber) {
         this.rolledNumber = rolledNumber;
     }
+
+  
+
+   
 }
