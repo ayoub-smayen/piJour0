384852bb294e42cd37e9488aff19bdb9966baa3c
eya122 <bahria.eya@gmail.com>
2021-04-12 14:16:25 +0100

    update profitservice

--- src/main/java/com/project0/esprit/service/ProfitService.java
+++ src/main/java/com/project0/esprit/service/ProfitService.java
@@ -6,14 +6,11 @@ import java.util.Map;
 import com.project0.esprit.entity.Profit;
 public interface ProfitService {
 
-	Profit addProfit(Profit d);
+	   Profit addProfit(Profit d);
 	   Profit updateProfit(Profit d);
 	   void deleteProfit(Long id);
 	   Profit retrieveProfit(Long id);
 	   HashMap<String, Object> getTodayRevenueDash();
-	//   Double getAllGain(Double income , Double outcome );
-	 //  HashMap<String, Object> getBestProduct();
-		
-	List<Map<String , Double>>  getgainmaithly();
+	   List<Map<String , Double>>  getgainmaithly();
 	   
 }
