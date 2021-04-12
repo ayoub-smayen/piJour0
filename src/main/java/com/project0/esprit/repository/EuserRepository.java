8265fe2d9e37d043d6c08fb89dd6fb8ae765b97d
eya122 <bahria.eya@gmail.com>
2021-04-12 14:13:54 +0100

    update euserrepository

--- src/main/java/com/project0/esprit/repository/EuserRepository.java
+++ src/main/java/com/project0/esprit/repository/EuserRepository.java
@@ -3,7 +3,6 @@ package com.project0.esprit.repository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-//import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -15,12 +14,7 @@ import com.project0.esprit.entity.Euser;
 @Repository
 public interface EuserRepository extends CrudRepository<Euser, Long> {
 
-	//List<User> findByFirstname(String membre_username);
-	@Transactional
-	/*@Modifying
-	@Query("UPDATE Euser SET membre_username = ?1 WHERE user_id = ?2")
-	public void updateUserByFirstName(@Param(value="membre_name") String membre_username , @Param(value="user_id" ) Long user_id);
-	*/
+//	@Transactional
 	@Query("SELECT a FROM Euser a WHERE user_id = ?1")
 	public Euser findByIdd(@Param(value="user_id" ) Long user_id);
 
