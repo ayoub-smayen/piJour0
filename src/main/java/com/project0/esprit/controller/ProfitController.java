f132305a7365309ee10bdab93a3c7a80d4278ded
eya122 <bahria.eya@gmail.com>
2021-04-12 13:01:00 +0100

    update profitController

--- src/main/java/com/project0/esprit/controller/ProfitController.java
+++ src/main/java/com/project0/esprit/controller/ProfitController.java
@@ -1,8 +1,6 @@
 package com.project0.esprit.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,14 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project0.esprit.entity.Product1;
 import com.project0.esprit.entity.Profit;
-import com.project0.esprit.repository.ProductRepository;
 import com.project0.esprit.service.ProfitService;
 
 
@@ -32,8 +27,6 @@ public class ProfitController {
 
 	@Autowired
 	private ProfitService profitservice;
-	@Autowired 
-	private ProductRepository productservice;
 	
 	//http://localhost:8091/api/profit/get/{profit_id}
 	@GetMapping("/profit/get/{profit_id}")
@@ -41,14 +34,6 @@ public class ProfitController {
 	public Profit retrieveProfit(@PathVariable("profit_id") Long id) {
 	return profitservice.retrieveProfit(id);
 	}
-	
-	//http://localhost:8091/api/profit/add
-	/*@PostMapping("/profit/add")
-	@ResponseBody
-	public Profit addProfit(@RequestBody Profit p) {
-	  Profit profit = profitservice.addProfit(p);
-	return profit;
-	}*/
 	
 	
 	//http://localhost:8091/api/profit/add
@@ -61,15 +46,9 @@ public class ProfitController {
 	}
 
 
-	//http://localhost:8091/api/profit/update/{profit_id}
-	/*@PutMapping("/profit/update/{profit_id}")
-	@ResponseBody
-	public Profit modifyProfit(@RequestBody Profit profit) {
-	return profitservice.updateProfit(profit);
-	}*/
 	
 	//http://localhost:8091/api/profit/update/{profit_id}
-	@PutMapping("/update")
+	@PutMapping("/profit/update/{profit_id}")
     public @ResponseBody ResponseEntity<Profit> updateProfit( Profit p)
 
     {
@@ -77,60 +56,14 @@ public class ProfitController {
 	return new ResponseEntity<Profit>(profitservice.updateProfit(p),HttpStatus.ACCEPTED);
     }
 	
+	
 	//http://localhost:8091/api/profit/delete/{profit_id}
-	@DeleteMapping("/profit/delete/{user_id}")
+	@DeleteMapping("/profit/delete/{profit_id}")
 	@ResponseBody
-	public void removeProfit(@PathVariable("user_id") Long id) 
+	public void removeProfit(@PathVariable("profit_id") Long id) 
 	{
 	profitservice.deleteProfit(id);
 	}
-	
-	
-	/*//http://localhost:8091/api/profit/profitpermonth
-	@GetMapping("/profitpermonth")
-	  
-	@ResponseBody public ResponseEntity<?>  getGainmonthly( ){
-		
-		List<Map<String, Double>> k = profitservice.getgainmaithly();
-	  return  ResponseEntity.status(HttpStatus.ACCEPTED).body(k);	
-	}*/
-  
-/////////////////////////////
-	/*@GetMapping("/profit/{income}/{outcome}")
-	  
-	@ResponseBody public ResponseEntity<?>  getGain(@PathVariable("income") Double income,@PathVariable("outcome") Double outcome ){
-		
-		Double k = profitservice.getAllGain(income, outcome);
-	  return  ResponseEntity.status(HttpStatus.ACCEPTED).body(k);	
-	}*/
-
-
-	/*@GetMapping("/profit2")
-	  
-	@ResponseBody public ResponseEntity<?> getGain2()
-	{
-		
-		HashMap<String, Object> k =  profitservice.getBestProduct();
-	  return  ResponseEntity.status(HttpStatus.ACCEPTED).body(k);	
-	}*/
-	
-/*	@GetMapping("/dashboard2")
-	//@Secured("ROLE_ADMIN")
-	private List<Product1>  getBestprod(){
-		List<Product1>  bestproduct =new ArrayList<>();
-		List<Product1> p1 = productservice.findAll();
-		
-		for (Product1  x: p1 ) {
-			
-			if(x.getQuantity() <20) {
-				bestproduct.add(x);
-			}
-			
-		}
-		
-		 return bestproduct;
-	
-	}*/
 	
 	
 
