cad8f4c12944d5091e372d85bff8855ec77816fc
eya122 <bahria.eya@gmail.com>
2021-04-12 12:34:42 +0100

    update userrepository

--- src/main/java/com/project0/esprit/repository/EuserRepository.java
+++ src/main/java/com/project0/esprit/repository/EuserRepository.java
@@ -17,18 +17,13 @@ public interface EuserRepository extends CrudRepository<Euser, Long> {
 
 	//List<User> findByFirstname(String membre_username);
 	@Transactional
-	@Modifying
+	/*@Modifying
 	@Query("UPDATE Euser SET membre_username = ?1 WHERE user_id = ?2")
 	public void updateUserByFirstName(@Param(value="membre_name") String membre_username , @Param(value="user_id" ) Long user_id);
-	
-	
+	*/
 	@Query("SELECT a FROM Euser a WHERE user_id = ?1")
 	public Euser findByIdd(@Param(value="user_id" ) Long user_id);
-///////////////
-	//@Query("SELECT b from bill b  join b.online_payment op join op.command c join c.user u where u.id=:userId")
-	
-	///bill mfamech zid chufha
-//////////	
+
 	@Query("select count(u)  from Euser  u  WHERE  u.city = ?1 ")
 	public int getCityCount(@Param(value = "city")  String city);
 	
